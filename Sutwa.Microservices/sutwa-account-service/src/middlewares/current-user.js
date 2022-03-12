abedf247f6f5de8a72180f52eab35eb9e4c1bff3
Janviermbilizi <janviermbilizi@gmail.com>
2022-03-11 21:44:10 -0500

    Removed comments

--- Sutwa.Microservices/sutwa-account-service/src/middlewares/current-user.js
+++ Sutwa.Microservices/sutwa-account-service/src/middlewares/current-user.js
@@ -1,11 +1,9 @@
 import jwt from "jsonwebtoken";
 
 const currentUser = (req, res, next) => {
-  //check if session or session with token exist
   if (!req.session?.token) {
     return next();
   }
-  //decode the token and check if it still valid
   try {
     const payload = jwt.verify(req.session.token, process.env.JWT_SECRET);
     req.currentUser = payload;
--- Sutwa.Microservices/sutwa-account-service/src/middlewares/require-auth.js
+++ Sutwa.Microservices/sutwa-account-service/src/middlewares/require-auth.js
@@ -1,7 +1,6 @@
 import { NotAuthorizedError } from "../errors/not-authorized-error.js";
 
 const requireAuth = (req, res, next) => {
-  //check if currentUser exist
   if (!req.currentUser) {
     const err = new NotAuthorizedError();
     return next(err);
