0a5960b299506c8356d930270848764ff84d5276
Janviermbilizi <janviermbilizi@gmail.com>
2022-06-27 00:25:13 -0400

    Created fake clubsdata to render in clubs homepage

---
+++ web-client/api/clubsData.js
@@ -0,0 +1,38 @@
+export const clubs = [
+  {
+    id: 1,
+    coverImage: "https://picsum.photos/1000/450?grayscale",
+    image: "https://picsum.photos/180?grayscale",
+    name: "New American FC",
+    people: "3.4K Friends . 12 Mutual",
+    //will add array of objects with image and url later that take you to the profile of the participant
+    fewParticipants: "Images Images",
+  },
+  {
+    id: 2,
+    coverImage: "https://picsum.photos/1000/450?grayscale",
+    image: "https://picsum.photos/180?grayscale",
+    name: "Real Madrid FC",
+    people: "3.4K Friends . 13 Mutual",
+    //will add array of objects with image and url later that take you to the profile of the participant
+    fewParticipants: "Images Images",
+  },
+  {
+    id: 3,
+    coverImage: "https://picsum.photos/1000/450?grayscale",
+    image: "https://picsum.photos/180?grayscale",
+    name: "Eagles BC",
+    people: "3.4K Friends . 25 Mutual",
+    //will add array of objects with image and url later that take you to the profile of the participant
+    fewParticipants: "Images Images",
+  },
+  {
+    id: 4,
+    coverImage: "https://picsum.photos/1000/450?grayscale",
+    image: "https://picsum.photos/180?grayscale",
+    name: "Barcelona FC",
+    people: "3.4K Friends . 100 Mutual",
+    //will add array of objects with image and url later that take you to the profile of the participant
+    fewParticipants: "Images Images",
+  },
+];
--- web-client/src/Components/Clubs/ClubsHomePage/index.js
+++ web-client/src/Components/Clubs/ClubsHomePage/index.js
@@ -1,5 +1,23 @@
 import * as React from "react";
+import { List } from "@mui/material";
+import Link from "next/link";
+import { clubs } from "../../../../api/clubsData";
 
 export function ClubsHomePage() {
-  return <div className="mt-5">Clubs Home Page</div>;
+  return (
+    <React.Fragment>
+      <h1 className="mt-5 pt-5">Clubs Database</h1>
+      <List className="row">
+        {clubs.map((club, index) => (
+          <Link
+            key={index}
+            href={`/clubs/${club.id}`}
+            className="col-12 text-center"
+          >
+            {club.name}
+          </Link>
+        ))}
+      </List>
+    </React.Fragment>
+  );
 }
